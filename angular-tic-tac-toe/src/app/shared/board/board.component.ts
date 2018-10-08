72cf97d57c377132f6ccf673872d06e7da20791d
Venkatesh Maharajan <connectwithvenkatesh@gmail.com>
2018-10-09 01:13:26 +0530

    responsive board

--- angular-tic-tac-toe/src/app/shared/board/board.component.ts
+++ angular-tic-tac-toe/src/app/shared/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -11,8 +11,27 @@ export class BoardComponent implements OnInit {
   @Input() size: number;
   numbers: Array<number>;
 
+  icons: Array<Array<string>>;
+
+  crossIcon: string = "close";
+  zeroIcon: string = "radio_button_unchecked";
+
+  turn: boolean;
+  
   ngOnInit() {
     this.numbers = Array.from(Array(this.size), (x, i) => i); 
+    this.icons = Array.from( Array(this.size), (x, i) => 
+      Array.from(Array(this.size), (y, j) => ``))
+    this.turn = true;
   }
 
+  onCellClick(i: number, j: number) {
+    if ( this.turn )
+      this.icons[i][j] = this.zeroIcon;
+    else 
+      this.icons[i][j] = this.crossIcon;
+
+    console.log( `Move: ${this.icons[i][j]} i=${i} j=${j}` );
+    this.turn = !this.turn;
+  }
 }
